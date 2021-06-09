e5a729f23f212da31c4a34eba48f35119e59b120
guillaume.erard <guillaume.erard@cgi.com>
2021-06-09 12:28:42 +0200

    jour 2 : juiste avant manger

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import Button from './components/Button/Button';
-
-//Composant principal de notre application 
-class App extends React.Component{
-  //counter = 1 
+import FlexLayout from './components/FlexLayout/FlexLayout';
+import MemeForm from './components/MemeForm/MemeForm';
+/**
+ * Composant principale de notre application
+ */
+class App extends React.Component {
+  //counter=1
   constructor(props){
     super(props);
-    this.state={counter:1, machaine:'Hello'};
+    this.state={current:{titre:'', x:0, y:0, text: '',image:0}};
   }
-  componentDidUpdate(){
+  componentDidUpdate(pprops,pstate){
     console.log(arguments);
     console.log(this.state);
+    //Verif et moddif d'etat suite a une modif d'etat (action en chaine) Attention prevenir mles boucles infinies
+    if(this.state.counter===1 && this.state.maChaine!=='new Val'){this.setState({maChaine:'new Val'});}
   }
   render() {
     return <div className="App">
-    Compter le counter : {this.state.counter}
-    <hr/>
-    {this.state.machaine}
-    <hr/>
-      <Button lorsqueJeCliqueraiSurLeButton={()=>{
-        console.log('J\'ai été cliqué sur ça : '+this.state.counter)
-        this.setState({counter:this.state.counter+1})
-        }}>
-        <img src="https://cdn0.iconfinder.com/data/icons/typicons-2/24/beer-512.png" alt='click'/>Need a beer ? Push the button !</Button>
-
-    </div>
+      <FlexLayout>
+          <div></div>
+          <MemeForm onSubmit={formState=>this.setState({current:formState})}/>
+      </FlexLayout>
+      {JSON.stringify(this.state)}
+     
+    </div>;
   }
 }
 export default App;
---
+++ app/src/App/components/MemeForm/MemeForm.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import styles from './MemeForm.module.css';
+
+const MemeForm = (props) => {
+
+  const [state, setstate] = useState({titre:'bla', x:10, y:20, text: 'coucou',image:1});
+  return (
+      <div className={styles.MemeForm} data-testid="MemeForm">
+        <form onSubmit={(evt)=>{
+          //Annulation du comportement par défaut de la soumission d'un formulaire
+          evt.preventDefault();
+          //Appel de la fonction envoyée par les props
+          props.onSubmit(state);
+        }}>
+          <label htmlFor="titre">Titre</label><br /><input onChange={evt=>{
+            setstate({...state, titre:evt.target.value})
+          }} value={state.titre} type="text" id="titre" placeholder="saisissez le titre" />
+          <hr />
+          <label htmlFor="image">image</label><br /><select value={state.image} id="image"><option onChange={evt=>{
+            setstate({...state, image:evt.target.value})
+          }} value="1">img.jpg</option></select>
+          <hr />
+          <label htmlFor="text">Texte</label><br /><input onChange={evt=>{
+            setstate({...state, text:evt.target.value})
+          }} value={state.text} type="text" id="text" />
+          <div style={{ textAlign: 'center' }}>
+            <div style={{ display: 'inline-block' }}><label htmlFor="x">X </label><br /><input onChange={evt=>{
+            setstate({...state, x:evt.target.value})
+          }} value={state.x} type="number" id="x" /></div>
+            <div style={{ display: 'inline-block' }}><label htmlFor="y">Y</label><br /><input onChange={evt=>{
+            setstate({...state, y:evt.target.value})
+          }} value={state.y} type="number" id="y" /></div>
+          </div>
+          <hr />
+          <input type="submit" value="Save"></input>
+          <input type="reset" value="Reset"></input>
+
+        </form>
+        {JSON.stringify(state)}
+      </div>
+    )
+  }
+
+MemeForm.propTypes = {onSubmit:PropTypes.func.isRequired};
+
+MemeForm.defaultProps = {};
+
+export default MemeForm;
